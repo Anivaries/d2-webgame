f74157aa52a65f6d2a5679de18ef0f66d81324cc
Anivaries <k0m4r4c00@gmail.com>
2023-07-16 22:40:49 +0200

    messy code, added js to ajax logic

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, game_1, get_data,   random_heroes
-# game_hero, show_data,
+from .views import index, game_1, get_data,   random_heroes, check_winner
+# game_hero, show_data, 
 
 urlpatterns = [
     path('', index, name="index"),
+    path('check_winner/', check_winner, name="check_winner"),
     # path('game/', game_hero, name="g1"),
     path('get_data/', get_data, name="get_data"),
     path('game/', game_1, name="game"),
--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from bs4 import BeautifulSoup as bs
 import requests
@@ -10,13 +10,25 @@ BRACKET_LIST = ["Herald", "Guardian", "Crusader", "Archon", "Legend", "Ancient",
 def index(request):
     return render(request, 'index.html')
 
-
 def game_1(request):
     return render(request, 'game_1.html')
+    # print(request.__dict__)
+    # sd = random_heroes(request)
+    # winner = sd[2]['winner']
+    # hero_a = sd[0]['out_1'][0]
+    # hero_b = sd[1]['out_2'][0]
+
+    
+
+    # context = {
+    #     'hero_a':hero_a,
+    #     'hero_b':hero_b
+    # }
+    # return render(request, 'game_1.html', context)
 
 def get_data(request):
     response = requests.get("https://www.dotabuff.com/heroes/meta?view=played&metric=rating_bracket", headers={'User-agent':'your bot 0.1'})
-    if response.status_code is not 200:
+    if response.status_code != 200:
         HttpResponse("Something went wrong")
     else:
         parsed_page = response.content
@@ -38,13 +50,67 @@ def get_data(request):
 
 def random_heroes(request):
 
+    
     with open ('data.json', 'r') as f:
             f = json.loads(f.read())
     hero_a = choice(list(f.items()))
     hero_b = choice([hero for hero in list(f.items()) if hero != hero_a])
-    return JsonResponse([{'out_1':hero_a},{'out_2':hero_b}], safe=False)
+    herald_wr_a = float(hero_a[1][0][:-1])
+    herald_wr_b = float(hero_b[1][0][:-1])
+    if herald_wr_a > herald_wr_b:
+        winner = hero_a[0]
+    else:
+        winner = hero_b[0]
+    chckfun(winner, kwargs=winner)
+    # print(winner)
+    return JsonResponse([{'out_1':hero_a},{'out_2':hero_b}, {'winner':winner}], safe=False)
+    # return [{'out_1':hero_a},{'out_2':hero_b}, {'winner':winner}]
+
+def chckfun(*args, **kwargs):
+    winner = args[0]
+    test_winner = winner
+    print(kwargs)
+
+    # print(winner)
+def check_winner(request):
+    if request.method == "POST":
+
+        pop = request.POST.get('hero_a_button')
+        popa = request.POST.get('hero_b_button')
+        # print(popa)
+        chckfun(pop, popa)
+    sd = random_heroes(request)
+    winner = sd[2]['winner']
+    hero_a = sd[0]['out_1'][0]
+    hero_b = sd[1]['out_2'][0]
+
+    
+
+    context = {
+        'hero_a':hero_a,
+        'hero_b':hero_b,
+        'request.POST':request.POST,
+        'request.POST':request.POST,
+    }
+    return render(request, 'game_1.html', context)
 
 
+
+
+# def get_next_pair():
+#     random_heroes()
+
+
+
+
+
+#     if request.method == "POST":
+#         if 'hero_a_button' in request.POST:
+#             print(request.POST['hero_a_button'])
+#             print("ASDASD")
+#         elif 'hero_b_button' in request.POST:
+#             print(request.POST['hero_b_button'])
+#     return game_1(request)
      
 
     #TO DO:
