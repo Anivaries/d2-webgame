37bf9f4ca448988aac368c14fef82480980df437
Anivaries <k0m4r4c00@gmail.com>
2023-07-17 17:44:24 +0200

    code fixes and updates

--- base/urls.py
+++ base/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path
-from .views import index, game_1, get_data,   random_heroes, check_winner
-# game_hero, show_data, 
+from .views import index, game_1, get_data, random_heroes
+# 
 
 urlpatterns = [
     path('', index, name="index"),
-    path('check_winner/', check_winner, name="check_winner"),
-    # path('game/', game_hero, name="g1"),
     path('get_data/', get_data, name="get_data"),
     path('game/', game_1, name="game"),
-    # path('show_data/', show_data, name="show_data"),
-    # path('/', check_answer, name="button"),
     path('random_heroes/', random_heroes, name="random_heroes"),
 ]
--- base/views.py
+++ base/views.py
@@ -12,20 +12,8 @@ def index(request):
 
 def game_1(request):
     return render(request, 'game_1.html')
-    # print(request.__dict__)
-    # sd = random_heroes(request)
-    # winner = sd[2]['winner']
-    # hero_a = sd[0]['out_1'][0]
-    # hero_b = sd[1]['out_2'][0]
 
-    
-
-    # context = {
-    #     'hero_a':hero_a,
-    #     'hero_b':hero_b
-    # }
-    # return render(request, 'game_1.html', context)
-
+# Download games data to JSON. Use this link once a week or so ( more often if patch is around )
 def get_data(request):
     response = requests.get("https://www.dotabuff.com/heroes/meta?view=played&metric=rating_bracket", headers={'User-agent':'your bot 0.1'})
     if response.status_code != 200:
@@ -50,7 +38,6 @@ def get_data(request):
 
 def random_heroes(request):
 
-    
     with open ('data.json', 'r') as f:
             f = json.loads(f.read())
     hero_a = choice(list(f.items()))
@@ -61,91 +48,10 @@ def random_heroes(request):
         winner = hero_a[0]
     else:
         winner = hero_b[0]
-    chckfun(winner, kwargs=winner)
-    # print(winner)
-    return JsonResponse([{'out_1':hero_a},{'out_2':hero_b}, {'winner':winner}], safe=False)
-    # return [{'out_1':hero_a},{'out_2':hero_b}, {'winner':winner}]
-
-def chckfun(*args, **kwargs):
-    winner = args[0]
-    test_winner = winner
-    print("JA SAM POZVAN")
-
-    # print(winner)
-def check_winner(request):
-    if request.method == "POST":
-
-        pop = request.POST.get('hero_a_button')
-        popa = request.POST.get('hero_b_button')
-        # print(popa)
-        chckfun(pop, popa)
-    sd = random_heroes(request)
-    winner = sd[2]['winner']
-    hero_a = sd[0]['out_1'][0]
-    hero_b = sd[1]['out_2'][0]
-
     
+    return JsonResponse([{'out_1':hero_a},{'out_2':hero_b}, {'winner':winner}], safe=False)
 
-    context = {
-        'hero_a':hero_a,
-        'hero_b':hero_b,
-        'request.POST':request.POST,
-        'request.POST':request.POST,
-    }
-    return render(request, 'game_1.html', context)
-
-
-
-
-# def get_next_pair():
-#     random_heroes()
-
-
-
-
-
-#     if request.method == "POST":
-#         if 'hero_a_button' in request.POST:
-#             print(request.POST['hero_a_button'])
-#             print("ASDASD")
-#         elif 'hero_b_button' in request.POST:
-#             print(request.POST['hero_b_button'])
-#     return game_1(request)
-     
 
     #TO DO:
-# DODAJ SKOR
+
 # STAVI MODAL DA PITA ZA BRACKET NA LOAD PAGE ODMA
-
-
-
-# def game_hero(request):
-
-#     with open ('data.json', 'r') as f:
-#         f = json.loads(f.read())
-#     hero_a = choice(list(f))
-#     hero_a_wr_hgc = float(f[hero_a][0][:-1])
-#     # Just excluding first picked hero so it doesn't show up twice 
-#     hero_b = choice([hero for hero in list(f) if hero != hero_a])
-#     hero_b_wr_hgc = float(f[hero_b][0][:-1])
-#     if hero_a_wr_hgc > hero_b_wr_hgc:
-#         winner = hero_a
-#     else:
-#         winner = hero_b
-
-#     context = {
-#         'hero_a':hero_a,
-#         'hero_a_wr_hgc':hero_a_wr_hgc,
-#         'hero_b':hero_b,
-#         'hero_b_wr_hgc':hero_b_wr_hgc,
-#         'winner':winner
-#     }
-#     return render(request, 'game_1.html', context)
-
-
-# def show_data(request):
-#     with open ('data.json', 'r') as f:
-#         f = json.loads(f.read())
-#     # hero_a = choice(list(f))
-#     # hero_b = choice([hero for hero in list(f) if hero != hero_a])
-#     return JsonResponse(f, safe=False)
